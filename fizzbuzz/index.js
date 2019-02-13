15ae537fe65c0a5e61f16ae14a4378123f91e55f
gxcad <gxcadr@gmail.com>
2019-02-12 21:15:34 -0800

    redid fizzbuzz and commit and push practice

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Write a program that console logs the numbers
+// Write a program that returns an array with the numbers
 // from 1 to n. But for multiples of three print
 // “fizz” instead of the number and for the multiples
 // of five print “buzz”. For numbers which are multiples
@@ -12,6 +12,17 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+const fizzbuzz = num => {
+  let result = [];
+  
+  for (let i = 1; i <= num; i += 1) {
+    if (i % 3 === 0 && i % 5 === 0) result.push('fizzbuzz');
+    else if (i % 3 === 0) result.push('fizz');
+    else if (i % 5 === 0) result.push('buzz');
+    else result.push(i);
+  }
+  
+  return result;
+}
 
-module.exports = fizzBuzz;
+console.log(fizzbuzz(20));
