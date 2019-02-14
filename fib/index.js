4c9d6f2eaf04584c61676ee3332ad1a208ed58e1
gxcad <gxcadr@gmail.com>
2019-02-14 07:51:45 -0800

    fib solution coded

--- fib/index.js
+++ fib/index.js
@@ -5,9 +5,25 @@
 // For example, the sequence
 //  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 // forms the first ten entries of the fibonacci series.
+
+// noted this problem has a questionable interpretation of what is the n-th number, so expected solutions are commented after each log test.
+// used my interpretation which is 0 is 1st, 1 is 2nd, 1 is 3rd, 2 is 4th, 3 is 5th, 5 is 6th, 8 is 7th, and so on.
+
+function fib(n) {
+  if (n === 1) return 0
+  let previous = 0;
+  let current = 1;
+  
+  for (let i = 2; i < n; i += 1) {
+    let temp = current;
+    current += previous;
+    previous = temp;
+  }
+  
+  return current;
+}
+
 // Example:
-//   fib(4) === 3
-
-function fib(n) {}
-
-module.exports = fib;
+console.log(fib(4)); // === 2
+console.log(fib(7)); // === 8
+console.log(fib(10)); // === 34
