90cdf2570407fc0e59d6012ff366e42858cfd948
gxcad <gxcadr@gmail.com>
2019-02-14 07:14:56 -0800

    attempt fix chunk

--- transformiteration/index.js
+++ transformiteration/index.js
@@ -41,8 +41,8 @@ const transform = (source, target) => {
 
 //examples / tests:
 console.log(transform('car','see')) // === ["car", "sar", "ser", "see"]
-// console.log(transform('floor','brake')) // === ["floor", "bloor", "broor", "braor", "brakr", "brake"]
-// console.log(transform('kata','math')) // === ["kata", "mata", "math"]
-// console.log(transform('deer','beer')) // === ['deer','beer']);
-// console.log(transform('a fall to the floor', 'braking the door in')) // === ['a fall to the floor','b fall to the floor','brfall to the floor','braall to the floor','brakll to the floor','brakil to the floor','brakin to the floor','brakingto the floor','braking o the floor','braking t the floor','braking ththe floor','braking thehe floor','braking the e floor','braking the d floor','braking the dofloor','braking the dooloor','braking the dooroor','braking the door or','braking the door ir','braking the door in']);
-// console.log(transform('Codewars','programs')) // === ["Codewars", "podewars", "prdewars", "proewars", "progwars", "progrars", "programs"]);
+console.log(transform('floor','brake')) // === ["floor", "bloor", "broor", "braor", "brakr", "brake"]
+console.log(transform('kata','math')) // === ["kata", "mata", "math"]
+console.log(transform('deer','beer')) // === ['deer','beer']);
+console.log(transform('a fall to the floor', 'braking the door in')) // === ['a fall to the floor','b fall to the floor','brfall to the floor','braall to the floor','brakll to the floor','brakil to the floor','brakin to the floor','brakingto the floor','braking o the floor','braking t the floor','braking ththe floor','braking thehe floor','braking the e floor','braking the d floor','braking the dofloor','braking the dooloor','braking the dooroor','braking the door or','braking the door ir','braking the door in']);
+console.log(transform('Codewars','programs')) // === ["Codewars", "podewars", "prdewars", "proewars", "progwars", "progrars", "programs"]);
