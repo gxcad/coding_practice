79b7bd11d6c661af488dc0bdc8fbb90e5dad4259
gxcad <gxcadr@gmail.com>
2019-02-14 06:57:55 -0800

    added reduce solution in comments

---
+++ transformiteration/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+nsole.log(transform('floor','brake')) // === ["floor", "bloor", "broor", "braor", "brakr", "brake"]
+// console.log(transform('kata','math')) // === ["kata", "mata", "math"]
+// console.log(transform('deer','beer')) // === ['deer','beer']);
+// console.log(transform('a fall to the floor', 'braking the door in')) // === ['a fall to the floor','b fall to the floor','brfall to the floor','braall to the floor','brakll to the floor','brakil to the floor','brakin to the floor','brakingto the floor','braking o the floor','braking t the floor','braking ththe floor','braking thehe floor','braking the e floor','braking the d floor','braking the dofloor','braking the dooloor','braking the dooroor','braking the door or','braking the door ir','braking the door in']);
+// console.log(t
