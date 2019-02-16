ec1dbf2bcffcf291d99cda2fb3eb068a93551345
gxcad <gxcadr@gmail.com>
2019-02-15 23:31:44 -0800

    added very clean one line recursion solution

--- squaresinrectangle/index.js
+++ squaresinrectangle/index.js
@@ -1,12 +1,14 @@
-function findSquares(x,y){
-  let total = x * y;
+// function findSquares(x,y){
+//   let total = x * y;
   
-  for (let i = 1; i < x && i < y; i += 1) {
-    total += (x - i) * (y - i);
-  }
+//   for (let i = 1; i < x && i < y; i += 1) {
+//     total += (x - i) * (y - i);
+//   }
 
-  return total;
-}
+//   return total;
+// }
+
+const findSquares = (x,y) => x&&y&&x * y + findSquares(x-1, y-1);
 
 console.log(findSquares(4, 3)); // 20
 console.log(findSquares(5, 5)); // 55
