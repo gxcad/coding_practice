b44fe807263f07e16fd13d729c1ca0deafef8d2f
gxcad <gxcadr@gmail.com>
2019-02-18 15:19:43 -0800

    meeting initial commit

---
+++ meeting/index.js
@@ -0,0 +1,8 @@
+function meeting(listOfNames) {
+  let names = listOfNames.toUpperCase().split(';');
+  // console.log(names);
+  
+}
+
+console.log(meeting("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn")); 
+// "(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)");))
