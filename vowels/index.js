fe82bd328a1fd607241a47b3613afb468d2a5869
gxcad <gxcadr@gmail.com>
2019-02-18 18:56:06 -0800

    vowels initial commit

--- vowels/index.js
+++ vowels/index.js
@@ -2,11 +2,19 @@
 // Write a function that returns the number of vowels
 // used in a string.  Vowels are the characters 'a', 'e'
 // 'i', 'o', and 'u'.
+
+function vowels(str) {
+  let vowels = 'aeiou';
+  let count = 0;
+  
+  for (letter of str) {
+    if ()
+  }
+}
+
 // --- Examples
-//   vowels('Hi There!') --> 3
-//   vowels('Why do you ask?') --> 4
-//   vowels('Why?') --> 0
+console.log(vowels('Hi There!')) // --> 3
+console.log(vowels('Why do you ask?')) // --> 4
+console.log(vowels('Why?')) // --> 0
 
-function vowels(str) {}
-
-module.exports = vowels;
+// module.exports = vowels;
