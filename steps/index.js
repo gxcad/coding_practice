6667314d31ba38a97017edf18eb47079f5d676fb
gxcad <gxcadr@gmail.com>
2019-02-18 19:23:29 -0800

    new solution for steps

--- steps/index.js
+++ steps/index.js
@@ -17,22 +17,34 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  if (n > 0) {
-    for (let i = 1; i <= n; i += 1) { //j has to run the number of times of i
-      let result = ''; // ##
-      for (let j = 1; j <= i; j += 1) {
-        result += '#';
-      }
-      for (let k = 1; k <= n - i; k += 1) { //k has to run n - i number of times
-        result += ' ';
-      }
-      console.log(result);
+const steps = n => {
+  for (let i = 1; i <= n; i += 1) {
+    let container = '';
+    for (let k = 1; k <= n; k += 1) {
+      if (k <= i) container += '#';
+      else container += ' ';
     }
+    console.log(container);
   }
 }
 
-console.log(steps(2));
+console.log(steps(4));
+
+// function steps(n) {
+//   if (n > 0) {
+//     for (let i = 1; i <= n; i += 1) { //j has to run the number of times of i
+//       let result = ''; // ##
+//       for (let j = 1; j <= i; j += 1) {
+//         result += '#';
+//       }
+//       for (let k = 1; k <= n - i; k += 1) { //k has to run n - i number of times
+//         result += ' ';
+//       }
+//       console.log(result);
+//     }
+//   }
+// }
+
 // module.exports = steps;
 
 // if (n > 0) {
