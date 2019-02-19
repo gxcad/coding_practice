1ee144cea97658584f78ddf07a68030f5f3b1fa7
gxcad <gxcadr@gmail.com>
2019-02-18 16:21:55 -0800

    completed solution for meeting

--- meeting/index.js
+++ meeting/index.js
@@ -1,7 +1,21 @@
 function meeting(listOfNames) {
   let names = listOfNames.toUpperCase().split(';');
-  // console.log(names);
+  let reversedNames = [];
+  let finalString = '';
+
+  for (name of names) {
+    let lastIndex = name.indexOf(':');
+    let lastName = name.slice(lastIndex + 1);
+    let firstName = name.slice(0, lastIndex);
+    reversedNames.push(lastName + ', ' + firstName);
+  }
+  reversedNames.sort();
   
+  for (name of reversedNames) {
+    finalString += '(' + name + ')';
+  }
+  
+  return finalString;
 }
 
 console.log(meeting("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn")); 
