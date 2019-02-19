26fb3631d461202e7a2a5914cb959d00a1310ab7
gxcad <gxcadr@gmail.com>
2019-02-18 21:33:04 -0800

    pyramid solution

--- pyramid/index.js
+++ pyramid/index.js
@@ -5,27 +5,31 @@
 // pyramid has spaces on both the left *and* right hand sides
 
 function pyramid(n) {
-  for (let row = 0; row < n; row++) {
+  for (let row = 1; row <= n; row++) {
     let string = '';
-    for (let column = 0; column < n; column++) {
-      if (row <= column) {
-        string += '#';
-      }
+    for (let column = 1; column <= n * 2 - 1; column++) {
+      if (column <= n - row || column > n + (row - 1)) string += ' ';
+      else string += '#';
     }
+    console.log(string);
   }
+
+  return;
 }
 
-// --- Examples
-console.log(pyramid(1));
-//       '#'
+// string.length === (n * 2 - 1)
 
-console.log(pyramid(2));
+// --- Examples
+// console.log(pyramid(1));
+//       '#' .length === 1 (n === 1)
+
+// console.log(pyramid(2));
 //       ' # '
-//       '###'
+//       '###' length === 3 (n === 2)
 
 console.log(pyramid(3));
 //       '  #  '
 //       ' ### '
-//       '#####'
+//       '#####' length === 5 (n === 3)
 
 // module.exports = pyramid;
