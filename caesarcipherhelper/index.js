560c3c4812c1005a869fba072d39b7eca0bafbc5
gxcad <gxcadr@gmail.com>
2019-02-24 13:01:31 -0800

    polybius cipher initial commit

--- caesarcipherhelper/index.js
+++ caesarcipherhelper/index.js
@@ -3,36 +3,43 @@
 // If something in the string is not in the alphabet (e.g. punctuation, spaces), simply leave it as is.
 // The shift will always be in range of [1, 26].
 
+// function CaesarCipher(shift) {
+//   let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//   this.encode = str => {
+//     let result = '';
+    
+//     for (char of str.toUpperCase()) {
+//       if (!alpha.includes(char)) result += char;
+//       else {
+//         let index = (alpha.indexOf(char) + shift) % 26;
+//         result += alpha[index];
+//       }
+//     }
+    
+//     return result;
+//   }
+
+//   this.decode = str => {
+//     let result = '';
+    
+//     for (char of str) {
+//       if (!alpha.includes(char)) result += char;
+//       else {
+//         let index = (alpha.indexOf(char) + 26 - shift) % 26;
+//         result += alpha[index];
+//       }
+//     }
+    
+//     return result;
+//   }
+// }
 
 function CaesarCipher(shift) {
   let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  this.encode = str => {
-    let result = '';
+  
+  this.encode = str => str.replace(/[a-z]/gi, char => alpha[(alpha.indexOf(char.toUpperCase()) + shift) % 26]);
     
-    for (char of str.toUpperCase()) {
-      if (!alpha.includes(char)) result += char;
-      else {
-        let index = (alpha.indexOf(char) + shift) % 26;
-        result += alpha[index];
-      }
-    }
-    
-    return result;
-  }
-
-  this.decode = str => {
-    let result = '';
-    
-    for (char of str) {
-      if (!alpha.includes(char)) result += char;
-      else {
-        let index = (alpha.indexOf(char) + 26 - shift) % 26;
-        result += alpha[index];
-      }
-    }
-    
-    return result;
-  }
+  this.decode = str => str.replace(/[a-z]/gi, char => alpha[(alpha.indexOf(char) + 26 - shift) % 26]);
 }
 
 // For example:
