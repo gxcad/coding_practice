dc94438d82f8535eab2d31bacbdcc4a0cee60bb5
gxcad <gxcadr@gmail.com>
2019-02-23 21:01:09 -0800

    CaesarCipher class solution completed

---
+++ caesarcipherhelper/index.js
@@ -0,0 +1,42 @@
+// Write a class that, when given a string, will return an uppercase string with each letter shifted forward in the alphabet by however many spots the cipher was initialized to.
+
+// If something in the string is not in the alphabet (e.g. punctuation, spaces), simply leave it as is.
+// The shift will always be in range of [1, 26].
+
+
+function CaesarCipher(shift) {
+  let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  this.encode = str => {
+    let result = '';
+    
+    for (char of str.toUpperCase()) {
+      if (!alpha.includes(char)) result += char;
+      else {
+        let index = (alpha.indexOf(char) + shift) % 26;
+        result += alpha[index];
+      }
+    }
+    
+    return result;
+  }
+
+  this.decode = str => {
+    let result = '';
+    
+    for (char of str) {
+      if (!alpha.includes(char)) result += char;
+      else {
+        let index = (alpha.indexOf(char) + 26 - shift) % 26;
+        result += alpha[index];
+      }
+    }
+    
+    return result;
+  }
+}
+
+// For example:
+
+let c = new CaesarCipher(5); // creates a CipherHelper with a shift of five
+console.log(c.encode('Codewars')); // returns 'HTIJBFWX'
+console.log(c.decode('BFKKQJX')); // returns 'WAFFLES'
