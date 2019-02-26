9d36f8700d069a8a9b005e7141019bbb698ae707
gxcad <gxcadr@gmail.com>
2019-02-25 20:53:05 -0800

    encrypt this initial commit

---
+++ encryptthis/index.js
@@ -0,0 +1,19 @@
+// Encrypt this!
+
+// You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+//     Your message is a string containing space separated words.
+//     You need to encrypt each word in the message using the following rules:
+//         The first letter needs to be converted to its ASCII code.
+//         The second letter needs to be switched with the last letter
+//     Keepin' it simple: There are no special characters in input.
+
+// Examples:
+
+const encryptThis = function(text) {
+  
+}
+
+console.log(encryptThis("Hello")); // === "72olle"
+console.log(encryptThis("good")); // === "103doo"
+console.log(encryptThis("hello world")); // === "104olle 119drlo"
