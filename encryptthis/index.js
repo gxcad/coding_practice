5589c45e088ed9ffa7aef5c3000e6568b8c22bf0
gxcad <gxcadr@gmail.com>
2019-02-25 22:25:44 -0800

    encryptthis solution

--- encryptthis/index.js
+++ encryptthis/index.js
@@ -11,7 +11,24 @@
 // Examples:
 
 const encryptThis = function(text) {
-  
+  let result = [];
+  let words = text.split(' ');
+  let encoded;
+
+  for (word of words) {
+    encoded = '';
+    for (let i = 0; i < word.length; i += 1) {
+      let char = word[i];
+      let lastChar = word[word.length - 1];
+      if (i === 0) encoded += char.charCodeAt();
+      else if (i === 1) encoded += lastChar;
+      else if (i === word.length - 1) encoded += word[1];
+      else encoded += char;
+    }
+    result.push(encoded);
+  }
+
+  return result.join(' ');
 }
 
 console.log(encryptThis("Hello")); // === "72olle"
