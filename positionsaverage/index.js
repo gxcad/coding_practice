f50b153edcd739678d91c20b28fd76178f33f7f4
gxcad <gxcadr@gmail.com>
2019-02-27 03:36:12 -0800

    working solution for positions average problem

--- positionsaverage/index.js
+++ positionsaverage/index.js
@@ -1,9 +1,20 @@
 function posAverage(s) {
   let array = s.split(', ');
+  let count = 0;
+  let multiplier = 0;
   
   for (let i = 0; i < array.length; i += 1) {
-    
+    for (let j = i + 1; j < array.length; j += 1) {
+      multiplier += 1;
+      for (let k = 0; k < array[i].length; k += 1) {
+        let num1 = array[i][k];
+        let num2 = array[j][k];
+        if (num1 === num2) count += 1;
+      }
+    }
   }
+  let solution = (count / (multiplier * array[0].length)) * 100;
+  return solution.toFixed(10);
 }
 
 console.log(posAverage("444996, 699990, 666690, 096904, 600644, 640646, 606469, 409694, 666094, 606490")); //29.2592592593
