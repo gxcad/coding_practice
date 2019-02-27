ff64f6da39f26f205dc6167e0793d7b491aa6d3a
gxcad <gxcadr@gmail.com>
2019-02-27 03:02:26 -0800

    positions average initial commit

---
+++ positionsaverage/index.js
@@ -0,0 +1,9 @@
+function posAverage(s) {
+  let array = s.split(', ');
+  
+  for (let i = 0; i < array.length; i += 1) {
+    
+  }
+}
+
+console.log(posAverage("444996, 699990, 666690, 096904, 600644, 640646, 606469, 409694, 666094, 606490")); //29.2592592593
