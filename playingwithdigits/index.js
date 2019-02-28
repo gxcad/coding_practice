6827e5be7409d4965c5b39c2d8805edeefd50b57
gxcad <gxcadr@gmail.com>
2019-02-28 00:00:01 -0800

    working solution for digpow

--- playingwithdigits/index.js
+++ playingwithdigits/index.js
@@ -18,12 +18,19 @@
 
 // Note: n and p will always be given as strictly positive integers.
 
+function digPow(n, p){
+  let nums = n.toString().split('');
+  let total = 0;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    total += Math.pow(nums[i], p+i);
+  }
+
+  if (total % n === 0) return total / n;
+  else return -1
+}
+
 console.log(digPow(89, 1)); // should return 1 since 8¹ + 9² = 89 = 89 * 1
 console.log(digPow(92, 1)); // should return -1 since there is no k such as 9¹ + 2² equals 92 * k
 console.log(digPow(695, 2)); // should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
 console.log(digPow(46288, 3)); // should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
-
-
-function digPow(n, p){
-  
-}
