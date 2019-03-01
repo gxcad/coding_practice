879c1e76e05f8cd69444e0184bee1663175423bd
gxcad <gxcadr@gmail.com>
2019-03-01 01:19:11 -0800

    divisibility by 13 working solution

--- divisibilityby13/index.js
+++ divisibilityby13/index.js
@@ -1,13 +1,21 @@
 function thirt(num) {
-  let numString = num.toString();
   let prevSum = 0;
-  let currentSum = 0;
+  let currentSum = num;
   let divisions = [1, 10, 9, 12, 3, 4];
-
-  for (let i = 1; i < numString.length; i += 1) {
-    let num1 = numString[numString.length - i];
+  
+  while (currentSum !== prevSum) {
+    let numString = currentSum.toString();
+    prevSum = currentSum;
+    currentSum = 0;
+    for (let i = 1; i <= numString.length; i += 1) {
+      let num1 = numString[numString.length - i];
+      let num2 = divisions[(i - 1) % 6];
+      currentSum += num1 * num2;
+    }
   }
+
+  return currentSum;
 }
 
 console.log(thirt(1234567)) // 87
-console.log(third(321)); // 48
+// console.log(thirt(321)); // 48
