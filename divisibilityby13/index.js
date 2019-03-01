15ebb774d364450595429389fcb4dda41c082133
gxcad <gxcadr@gmail.com>
2019-03-01 00:21:56 -0800

    divisibility by 13 initial commit

---
+++ divisibilityby13/index.js
@@ -0,0 +1,13 @@
+function thirt(num) {
+  let numString = num.toString();
+  let prevSum = 0;
+  let currentSum = 0;
+  let divisions = [1, 10, 9, 12, 3, 4];
+
+  for (let i = 1; i < numString.length; i += 1) {
+    let num1 = numString[numString.length - i];
+  }
+}
+
+console.log(thirt(1234567)) // 87
+console.log(third(321)); // 48
