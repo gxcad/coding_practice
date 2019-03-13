a6db2c7d8319546009d33b76e442714e41cc081f
gxcad <gxcadr@gmail.com>
2019-03-12 19:53:52 -0700

    linked list solution for 5th test

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -17,7 +17,35 @@ class LinkedList {
       if (this.head) node1.next = this.head;
       this.head = node1;
     };
+    this.size = function() {
+      let count = 0;
+      while (this.head) {
+        count += 1;
+        this.head = this.head.next;
+      }
+
+      return count;
+    }
   }
 }
 
 module.exports = { Node, LinkedList };
+
+// let list1 = new LinkedList();
+// ----------
+// node1 {
+//   data: '35',
+//   next: 'node2'  (head)
+// }
+
+// node2 {
+//   data: '35',
+//   next: 'node3'
+// }
+
+// node3 {
+//   data: '35',
+//   next: null
+// }
+// --------
+// list.size() return the number of nodes currently in the list.
