e4d16cb8be0ce84a388d2b12fc56abf9c0fc2c24
gxcad <gxcadr@gmail.com>
2019-04-21 20:14:15 -0700

    union and one solution

--- intersection/index.js
+++ intersection/index.js
@@ -2,7 +2,7 @@
 
 // Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs.BONUS - Use reduce!
 
-const intersection = arrs => arrs.reduce((acc, arr) => acc.filter(num => arr.includes(num)));
+// const intersection = arrs => arrs.reduce((acc, arr) => acc.filter(num => arr.includes(num)));
 
 // Alternate solution:
 // let result = [];
@@ -15,7 +15,10 @@ const intersection = arrs => arrs.reduce((acc, arr) => acc.filter(num => arr.inc
 // return result;
 
 // Uncomment these to check your work!
-const arr1 = [5, 10, 15, 20];
-const arr2 = [15, 88, 1, 5, 7];
-const arr3 = [1, 10, 15, 5, 20];
-console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
+// const arr1 = [5, 10, 15, 20];
+// const arr2 = [15, 88, 1, 5, 7];
+// const arr3 = [1, 10, 15, 5, 20];
+// console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
+
+const union = arrays => {
+  return arrays.reduce((initial, curArr) => initial.filter(num => num.includes(initial)).concat(curArr.filter(num => initial.includes(num))));
